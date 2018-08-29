560fd42bc6f3854689114a7633b59d7521e05e66
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2018-08-29 07:54:24 -0300

    Constant support.

--- chunk.c
+++ chunk.c
@@ -7,6 +7,7 @@ void initChunk(Chunk* chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void writeChunk(Chunk* chunk, uint8_t byte) {
@@ -24,4 +25,10 @@ void writeChunk(Chunk* chunk, uint8_t byte) {
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
   initChunk(chunk);
+  freeValueArray(&chunk->constants);
+}
+
+int addConstant(Chunk* chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
 }
