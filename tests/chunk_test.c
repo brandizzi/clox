102a137c4d77996cd809e632eb2bca6ac221a840
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2018-10-29 07:48:34 -0300

    more tests.

--- tests/chunk_test.c
+++ tests/chunk_test.c
@@ -27,6 +27,32 @@ void testGetTwoCommands(CuTest *tc) {
   freeChunk(&chunk);
 }
 
+void testGetManyCommands(CuTest *tc) {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, OP_RETURN, 456);
+  writeChunk(&chunk, OP_RETURN, 456);
+  writeChunk(&chunk, OP_RETURN, 456);
+  writeChunk(&chunk, OP_RETURN, 456);
+
+  CuAssertIntEquals(tc, 123, getLine(&chunk, 0));
+  CuAssertIntEquals(tc, 123, getLine(&chunk, 1));
+  CuAssertIntEquals(tc, 123, getLine(&chunk, 2));
+  CuAssertIntEquals(tc, 123, getLine(&chunk, 3));
+  CuAssertIntEquals(tc, 456, getLine(&chunk, 4));
+  CuAssertIntEquals(tc, 456, getLine(&chunk, 5));
+  CuAssertIntEquals(tc, 456, getLine(&chunk, 6));
+  CuAssertIntEquals(tc, 456, getLine(&chunk, 7));
+
+  freeChunk(&chunk);
+}
+
 void testGetTwoCommandsOneLine(CuTest *tc) {
   Chunk chunk;
   initChunk(&chunk);
